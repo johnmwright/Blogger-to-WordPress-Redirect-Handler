be8eb2fe3e0928b9f291639a29fc9278127e29f6
Matt Richmond <matt@crowdfavorite.com>
2012-03-15 00:19:55 -0600

    adding plugin and readme.txt

---
+++ README.txt
@@ -0,0 +1,24 @@
+# Blogger Redirect Handler
+
+## Description
+
+When migrating from Blogger to WordPress, the importer creates a `blogger_permalink` meta key for each post.  This plugin catches $_GET query parameters and does a lookup for a post with the matching postmeta, then redirects to it.
+
+## Installation
+
+1. Import blogger posts from Blogger to WordPress using the [Blogger Importer](http://wordpress.org/extend/plugins/blogger-importer/)
+2. Install this WordPress plugin in typical plugins folder, then activate
+3. Modify Blogger template to redirect to your WordPress blog, using JavaScript snippet below (change "example.com" to your domain).  [Screenshot](https://skitch.com/bigdawggi/8k8k2/blogger-redirect)
+	
+	<script type='text/javascript'>
+		var blogRoot=&quot;http://example.com/&quot;;
+		var Redir = (document.location.pathname == &#39;/&#39;) ? blogRoot : blogRoot + &quot;?blogger_redirect=&quot;+document.location.pathname.toLowerCase();
+			
+		document.location.href = Redir;
+	</script>
+	
+
+## Changelog
+
+### 1.0 
+- Initial Release
---
+++ blogger-redirect-handler.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Name: Blogger Redirect Handler
+Plugin URI: 
+Description: Receives redirects from a blogger blog, and looks up the corresponding post by meta key.  Default to check for "blogger_permalink" as the meta key.
+Version: 1.0
+Author: Matthew Richmond
+Author URI: http://matthewgrichmond.com
+*/
+
+class mgr_Blogger_Redirect_Handler {
+	// Which variable are we looking for -- coming from blogger?
+	var $get_var = 'blogger_redirect';
+	
+	/**
+	 * Adds the various actions
+	 */
+	function __construct() {
+		$this->blogger_meta = apply_filters'blogger_permalink';
+		
+		if (!empty($_GET[$this->get_var]) && !headers_sent()) {
+			add_action('init', array($this, 'possible_redirect'), 1);
+		}
+	}
+	
+	/**
+	 * Does the lookup to see if we have a post with that post meta
+	 */
+	function possible_redirect() {
+		// Clean our request
+		$search_link = strip_tags(stripslashes($_GET[$this->get_var]));
+		
+		// Look for our post
+		$posts = get_posts(array(
+			'meta_key' => apply_filters('blogger_meta_key', 'blogger_permalink', $this),
+			'meta_value' => $search_link,
+			'numberposts' => 1,
+		));
+		
+		if (!empty($posts)) {
+			$post = array_shift($posts);
+			wp_redirect(get_permalink($post->ID), 301);
+			exit;
+		}
+	}
+}
+new mgr_Blogger_Redirect_Handler;
+?>
